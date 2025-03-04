a44d330751a9cd7223e041b183148d46f05b04db
alessiotegoni <110291544+alessiotegoni@users.noreply.github.com>
2025-03-04 21:05:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,17 @@ async function getChefBirthday(id) {
     const recipeReq = await fetch(`https://dummyjson.com/recipes/${id}`)
     const recipe = await recipeReq.json()
       
-    if (!recipe) throw new Error("Recipe not found")
+    if (recipe?.message) throw new Error(recipe.message)
     
     const userReq = await fetch(`https://dummyjson.com/users/${recipe.userId}`)
-    const { birthDate } = await userReq.json()
+    const user = await userReq.json()
+
+    if (user?.message) throw new Error(user.message)
     
      const chefBirthday = new Date(birthDate).toLocaleDateString()
          
     return chefBirthday
+    
   } catch (err) {
     console.error(err)
   }
